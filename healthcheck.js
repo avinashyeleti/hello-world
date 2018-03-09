af10bcf5fbcaa6811b27d17d2ef2312dfec03db1
avinash.yeleti@gmail.com <GitHub123>
2018-03-09 16:56:17 -0500

    commit file.

--- healthcheck.js
+++ healthcheck.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export1
 export const ping = (req, res, next) => {
-  console.log("1234567");
+  console.log("ABCDEFGHIJK");
   export const ping = (req, res, next) => {
   res.status(200).json({ message: 'pong' });
 };
