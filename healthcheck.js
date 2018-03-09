fcf6db122716c8a6b7d943ccb2a59ce329c8b30c
avinashyeleti <35379916+avinashyeleti@users.noreply.github.com>
2018-03-09 13:36:18 -0500

    Update healthcheck.js

--- healthcheck.js
+++ healthcheck.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export1
+// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export// eslint-disable-next-line import/prefer-default-export
 // eslint-disable-next-line import/prefer-default-export
 export const ping = (req, res, next) => {
   res.status(200).json({ message: 'pong' });
